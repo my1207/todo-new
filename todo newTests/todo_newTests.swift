fd0c8076c96973edfa809238696db3c7e1cb375a
松田勇樹 <matsudayuuki@myuki-macbook-air.local>
2018-10-27 16:26:30 +0900

    Initial Commit

---
+++ todo newTests/todo_newTests.swift
@@ -0,0 +1,34 @@
+//
+//  todo_newTests.swift
+//  todo newTests
+//
+//  Created by 松田勇樹 on 2018/10/27.
+//  Copyright © 2018 松田勇樹. All rights reserved.
+//
+
+import XCTest
+@testable import todo_new
+
+class todo_newTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
