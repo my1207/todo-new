9a5b237348f093fba26929dfdb6e52305d2290cb
松田勇樹 <matsudayuuki@myuki-macbook-air.local>
2018-11-10 20:25:31 +0900

    todo first commit

---
+++ todo new/TodoListViewController.swift
@@ -0,0 +1,117 @@
+//
+//  ViewController.swift
+//  todo new
+//
+//  Created by 松田勇樹 on 2018/10/27.
+//  Copyright © 2018 松田勇樹. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    class Item {
+        var title : String
+        var done: Bool = false
+        
+        init(title: String) {
+            self.title = title
+        }
+    }
+    
+    // この配列に作ったアイテムを追加していく
+    var itemArray: [Item] = []
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        // NaviBarのタイトルを大きく表示させる
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // あらかじめ3つアイテムを作っておく
+        
+        
+        // 配列に追加
+   
+        
+    }
+    
+    
+    
+    // MARK - セルの数を指定
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArray.count
+        
+    }
+    
+    
+    
+    // MARK - セルのカスタマイズ
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 選択されたセルに実行される処理
+        
+        let item = itemArray[indexPath.row]
+        
+        // チェックマーク
+        item.done = !item.done
+        
+        // リロードしてUIに反映
+        self.tableView.reloadData()
+        
+        // セルを選択した時の背景の変化を遅くする
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        // プラスボタンが押された時に実行される処理
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "追加", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "リストに追加", style: .default) { (action) in
+            // 「リストに追加」を押された時に実行される処理
+            
+            let newItem: Item = Item(title: textField.text!)
+            
+            // アイテム追加処理
+            self.itemArray.append(newItem)
+            self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "新しいアイテム"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        // アイテム削除処理
+        itemArray.remove(at: indexPath.row)
+        let indexPaths = [indexPath]
+        tableView.deleteRows(at: indexPaths, with: .automatic)
+        
+    }
+    
+    
+
+    
+}
